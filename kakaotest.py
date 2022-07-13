034e981ee9b50a7d4f30b31ff139c23958eb211d
woococo <j2922y@gmail.com>
2022-07-13 16:18:38 +0900

    commit

--- kakaotest.py
+++ kakaotest.py
@@ -7,9 +7,11 @@ data = {
     "grant_type" : "authorization_code",
     "client_id" : "b57cdaa5057046acf918df0fefdb228c", # 가장 첫 화면에 있던 REST API 키 입력
     "redirect_uri" : "https://localhost.com",
-    "code"         : "sKaJJPYpll9w1UiAqgdB3D_wY_0SxPBxPmfzaQ2qv97hjUUDO1hH-8YMLTVvzkpLLgiazwo9cusAAAGB8SQYBg" # 방금 받은 코드 키 입력
+    "code"         : "Lz6SMSs3nEH4IAZukxGihPpfnvl6DfIqJTh2DSOoXpy_HPZat9mqKaBt6Qc5asFWArOoAgopb7gAAAGB9j1T9g" # 방금 받은 코드 키 입력
 
 }
 response = requests.post(url, data=data)
 tokens = response.json()
 print(tokens)
+
+# https://kauth.kakao.com/oauth/authorize?client_id=b57cdaa5057046acf918df0fefdb228c&response_type=code&redirect_uri={REDIRECT_URI}
