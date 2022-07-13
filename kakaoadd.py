900bb47a7a2e8f58c03a542aa826c591771b0245
woococo <j2922y@gmail.com>
2022-07-13 16:18:27 +0900

    commit

--- kakaoadd.py
+++ kakaoadd.py
@@ -7,7 +7,7 @@ url = "https://kauth.kakao.com/oauth/token"
 data = {
     "grant_type": "refresh_token",
     "client_id" : "b57cdaa5057046acf918df0fefdb228c",  
-    "refresh_token" : 'or_jTlQGMW2cu7y-9j1nKfE5qk8bkvdqkULuBYmsCj11nAAAAYHxJ5hn'  # 이번에는 갱신 토큰을 넣어 준다. 
+    "refresh_token" : 'pPBClccxPCCzJ36Imdt8oe_r_RKoif-AzaZ3sC7jCilvuQAAAYH2PqHU'  # 이번에는 갱신 토큰을 넣어 준다. 
 }
 
 response = requests.post(url, data=data)
